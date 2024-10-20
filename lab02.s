17c6e13d63d810bef2393cf3725614e1eefc148a
andxros <149706968+andxros@users.noreply.github.com>
2024-10-20 17:34:41 +0300

    Completed Lab02

--- lab02.s
+++ lab02.s
@@ -13,6 +13,30 @@ prog:
 # Write your code here!
 # Do not remove the prog label or write code above it!
 #-----------------------------
+
+    # -- Registers -- 
+    #    a0: address of the first array element
+    #    a1: array length
+    #    a2: the number that we are searching 
+    #    s0: final result / address
+    #    t0: loop counter
+    #    t1: array element value 
+
+            add    s0, zero, zero
+            add    t0, zero, zero
+    
+for_loop: 
+            beq    t0, a1, exit_for_loop
+            addi   t0, t0, 1           # increment counter by 1
+            lw     t1, 0(a0)           # t1 = Memory[a0]
+if:
+            bne    t1, a2 , exit_if    # if t1 == a2 then
+            add    s0, a0, zero        # save the new address to s0
+exit_if: 
+            addi   a0, a0, 4           # next array element address 
+            j      for_loop
+exit_for_loop:
+    
 done:
     addi a7, zero, 10 
     ecall
